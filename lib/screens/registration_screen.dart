3a70261e4d9822d5acd73bf2921763981e22fdde
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-06-24 18:05:57 +0530

    Add screen routes

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class RegistrationScreen extends StatefulWidget {
+  static const String id = '/register_screen';
+
   @override
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
