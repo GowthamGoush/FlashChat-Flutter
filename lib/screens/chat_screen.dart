32930d363a556745dd90939d035ddaf12f5179da
GowthamGoush <westcity.21.gowtham@gmail.com>
2021-07-25 19:58:46 +0530

    Update chat layout and feature

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flash_chat/components/bubble_message.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:loading_overlay/loading_overlay.dart';
 
 class ChatScreen extends StatefulWidget {
   static const String id = '/chat_screen';
@@ -11,7 +14,12 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _auth = FirebaseAuth.instance;
+  final _fireStore = FirebaseFirestore.instance;
+  final messageTextController = TextEditingController();
+  bool showProgressBar = false;
+
   User currentUser;
+  String text;
 
   @override
   void initState() {
@@ -45,37 +53,79 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text('⚡️Chat'),
         backgroundColor: Colors.lightBlueAccent,
       ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              decoration: kMessageContainerDecoration,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: TextField(
-                      onChanged: (value) {
-                        //Do something with the user input.
-                      },
-                      decoration: kMessageTextFieldDecoration,
-                    ),
+      body: LoadingOverlay(
+        isLoading: showProgressBar,
+        child: SafeArea(
+          child: Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                StreamBuilder<QuerySnapshot>(
+                  stream: _fireStore.collection('messages').snapshots(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      showProgressBar = true;
+                      return Center();
+                    } else {
+                      showProgressBar = false;
+                    }
+
+                    final messages = snapshot.data.docs.reversed;
+                    List<BubbleMessage> bubbleMessages = [];
+
+                    for (var message in messages) {
+                      final text = message.get('text');
+                      final sender = message.get('sender');
+
+                      bubbleMessages.add(BubbleMessage(
+                        text: text,
+                        sender: sender,
+                        isUser: sender == currentUser.email,
+                      ));
+                    }
+
+                    return Expanded(
+                      child: ListView(
+                        reverse: true,
+                        children: bubbleMessages,
+                      ),
+                    );
+                  },
+                ),
+                Container(
+                  alignment: Alignment.bottomCenter,
+                  decoration: kMessageContainerDecoration,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        child: TextField(
+                          controller: messageTextController,
+                          onChanged: (value) {
+                            text = value;
+                          },
+                          decoration: kMessageTextFieldDecoration,
+                        ),
+                      ),
+                      FlatButton(
+                        onPressed: () {
+                          _fireStore
+                              .collection("messages")
+                              .add({'text': text, 'sender': currentUser.email});
+                          messageTextController.clear();
+                        },
+                        child: Text(
+                          'Send',
+                          style: kSendButtonTextStyle,
+                        ),
+                      ),
+                    ],
                   ),
-                  FlatButton(
-                    onPressed: () {
-                      //Implement send functionality.
-                    },
-                    child: Text(
-                      'Send',
-                      style: kSendButtonTextStyle,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
